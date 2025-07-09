cf7631e90ea640d77dbcd12d698d02f41ffc6563
KentCbu <apmac2025@gmail.com>
2025-07-09 15:10:46 +0800

    Update shorten.js

--- api/shorten.js
+++ api/shorten.js
@@ -1,20 +1,46 @@
-const fs = require('fs');
-const path = require('path');
-const urlsPath = path.join(__dirname, '../../data/urls.json');
+const axios = require('axios');
+const crypto = require('crypto');
 
-exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+const BIN_ID = "686e1231dfff172fa657ed90";
+const API_KEY = "$2a$10$hMAe7sa9n3owoAS9NYMjUeizXEKs5k8wuNljsyVfGUuBXLLF1lz0G";
+
+exports.handler = async function(event) {
+  const body = JSON.parse(event.body);
+  const originalUrl = body.url;
+
+  if (!originalUrl) {
+    return { statusCode: 400, body: "Missing URL" };
   }
 
-  const { url } = JSON.parse(event.body);
-  const id = Math.random().toString(36).substring(2, 8);
-  const urls = JSON.parse(fs.readFileSync(urlsPath));
-  urls[id] = url;
-  fs.writeFileSync(urlsPath, JSON.stringify(urls, null, 2));
+  const shortId = crypto.randomBytes(3).toString('hex');
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ shortUrl: `https://watch-gomotv.online/${id}` })
-  };
+  try {
+    // Get current data from JSONBin
+    const getRes = await axios.get(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
+      headers: {
+        'X-Master-Key': API_KEY
+      }
+    });
+
+    const urls = getRes.data.record || {};
+    urls[shortId] = originalUrl;
+
+    // Save back to JSONBin
+    await axios.put(`https://api.jsonbin.io/v3/b/${BIN_ID}`, urls, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Master-Key': API_KEY
+      }
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ short: shortId })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
 };
